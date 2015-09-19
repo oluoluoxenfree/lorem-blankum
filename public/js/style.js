0793d6dfdcbe40a981fe3c6ce809c6d481864481
Olu <o.niyiawosusi@gmail.com>
2015-09-19 19:28:26 +0100

    fixed twitter client, started to add front end text box

--- public/js/style.js
+++ public/js/style.js
@@ -1,16 +1,3 @@
-var words = [],
-    envVars,
-    client,
-    Twitter = require('node-twitter'),
-    config = require( './config.json');
-
-var client = new Twitter({
-  consumer_key: config.cKey,
-  consumer_secret: config.cSecret,
-  access_token_key: config.atKey,
-  access_token_secret: config.atSecret
-});
-
 $(document).ready(function() {
 
   $(function(){
@@ -23,17 +10,5 @@ $(document).ready(function() {
   $('span').click(function() {
     $('span').remove();
     $('input').show();
-  });
-
-  $( ".search-term" ).keypress(function( event ) {
-    var searchTerm = $('.search-term').val();
-    if ( event.which == 13 ) {
-      var params = {screen_name: searchTerm};
-      client.get('statuses/user_timeline', params, function(error, tweets, response){
-        if (!error) {
-          console.log(tweets);
-        }
-      });
-    }
   });
 });
