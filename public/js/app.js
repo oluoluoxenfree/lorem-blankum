3c62fe6407a328bf90b35b83e24d6fdb6a97ab14
Olu <o.niyiawosusi@gmail.com>
2015-09-23 23:37:02 +0100

    tweets finally delivered to client side!!!

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
   $( ".search-term" ).keypress(function( event ) {
+    //remove # and @ from the search string
     var searchTerm = $('.search-term').val();
     if ( event.which == 13 ) {
       $.ajax({
         type: 'GET',
         data: {screen_name: searchTerm, include_rts: false},
         url: './' + searchTerm
-      }).done($('ipsum-area').text(JSON.stringify(tweets)));
+      }).done(function(tweets) {
+        $('.ipsum-area').text(JSON.stringify(tweets));
+      });
     }
   });
 });
