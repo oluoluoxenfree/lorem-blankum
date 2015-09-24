cffbfcea69cd56416dd4c30df0a31e665f61db33
Olu <o.niyiawosusi@gmail.com>
2015-09-24 01:26:24 +0100

    broken tweet sorting

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+var sentences = [];
   $( ".search-term" ).keypress(function( event ) {
     //remove # and @ from the search string
     var searchTerm = $('.search-term').val();
@@ -9,8 +9,16 @@ $(document).ready(function() {
         data: {screen_name: searchTerm, include_rts: false},
         url: './' + searchTerm
       }).done(function(tweets) {
-        $('.ipsum-area').text(JSON.stringify(tweets));
+        $('.ipsum-area').text(sortWords(tweets));
       });
     }
   });
 });
+
+function sortWords(tweets) {
+  for(var i = 0; i++; i < tweets.length) {
+    sentences.push(tweets[i].text);
+    console.log(sentences);
+    return sentences;
+  }
+}
