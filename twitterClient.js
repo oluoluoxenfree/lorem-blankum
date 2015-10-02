218cb339b26cce20c5bbd440c55deb94f5c977b5
Olu <o.niyiawosusi@gmail.com>
2015-10-02 22:24:37 +0100

    hosting on heroku take 4

--- twitterClient.js
+++ twitterClient.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var Twitter = require('twitter')
+var Twitter = require('twitter');
 
 try {
-    config = require( './config.json')
+  var config = require( './config.json');
+  var envars = {
+    consumer_key: config.cKey,
+    consumer_secret: config.cSecret,
+    access_token_key: config.atKey,
+    access_token_secret: config.atSecret
+  };
 }
 
 catch(err) {
     client = new Twitter({
-      consumer_key: config.cKey || process.env.C_KEY,
-      consumer_secret: config.cSecret || process.env.C_SECRET,
-      access_token_key: config.atKey || process.env.AT_KEY,
-      access_token_secret: config.atSecret || process.env.AT_SECRET
+      consumer_key: process.env.C_KEY,
+      consumer_secret: process.env.C_SECRET,
+      access_token_key: process.env.AT_KEY,
+      access_token_secret: process.env.AT_SECRET
     });
 }
 
