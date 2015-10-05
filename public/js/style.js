c2b41cebfeb9a55681bb0f88a9e233e8a0c0537d
Olu <o.niyiawosusi@gmail.com>
2015-10-06 00:00:16 +0100

    formatting

--- public/js/style.js
+++ public/js/style.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       typeSpeed: 0,
       callback: function() {
                   $('span').remove();
-                    $('input').show();
+                  $('input').show();
       }
     });
   });
